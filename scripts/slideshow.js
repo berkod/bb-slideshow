faf840866dbc4608ea2acdfb9feae31ee57c8a19
Danylo Berko <danylo_berko@condenast.com>
2015-05-12 23:45:30 -0400

    sketch out models

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,3 +1,30 @@
 /**
  * Created by dberko on 5/12/15.
  */
+var Slideshow = Backbone.Collection.extend({
+  model    : Slide
+  ,
+  nextOrder: function () {
+    if (!this.length) return 1;
+    return this.last().get('order') + 1;
+  }
+});
+
+
+Slide = Backbone.Model.extend({
+
+    // Default attributes for the todo item.
+    defaults: function () {
+      return {
+        title  : "",
+        summary: "",
+        link   : "",
+        postID : 0,
+        order  : gSlideshow.nextOrder(),
+      };
+    }
+  }
+);
+
+//global slideshow
+var gSlideshow = new Slideshow();
